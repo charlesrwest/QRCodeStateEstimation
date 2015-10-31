4c261dfe7b0f5e350f0524db574f85d6849dc05a
Charles West <crwest@ncsu.edu>
2015-10-31 11:20:22 -0400

    Added branch which will compile with opencv 3.0 (old version had compatability issue due to Opencv breaking highgui video processing functionality into a different module).

--- src/library/QRCodeStateEstimator.hpp
+++ src/library/QRCodeStateEstimator.hpp
@@ -11,6 +11,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/videoio.hpp>
 #include <zbar.h>
 
 //Declare handy constants
